8c71b99c83babba9c8004ac35a0f116db7679989
srakuma <robben2159@gmail.com>
2023-01-11 14:58:54 +0900

    fix context

--- parser/Context.cpp
+++ parser/Context.cpp
@@ -1,4 +1,5 @@
 #include "Context.hpp"
+#include <stdexcept>
 
 Context::Context()
 :_p(static_cast<Parser *>(NULL))
@@ -26,5 +27,8 @@ ParsingStatus Context::execParse(
 	const std::vector<std::pair<Symbol, std::string> > & tokens
 )
 {
+	if (_p == static_cast<Parser *>(NULL)) {
+		throw std::runtime_error("Parser is not set");
+	}
 	return _p->parse(tokens);
 }
