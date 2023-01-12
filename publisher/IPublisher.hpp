bee1e35b412b847219e1b881948fde2611b6bc89
srakuma <robben2159@gmail.com>
2023-01-12 20:12:13 +0900

    change name

---
+++ publisher/IPublisher.hpp
@@ -0,0 +1,16 @@
+#ifndef IPUBLISHER_HPP
+#define IPUBLISHER_HPP
+
+class ISubscriber;
+
+class IPublisher
+{
+public:
+	IPublisher();
+	~IPublisher();
+	virtual void notify(int) = 0;
+	virtual void subscribe(ISubscriber *) = 0;
+	virtual void unsubscribe(ISubscriber * ) = 0;
+};
+
+#endif /* IPUBLISHER_HPP */
---
+++ subscriber/ISubscriver.hpp
@@ -0,0 +1,12 @@
+#ifndef ISUBSCRIBER_HPP
+#define ISUBSCRIBER_HPP
+
+class ISubscriber
+{
+public:
+	ISubscriber();
+	~ISubscriber();
+	virtual void update(int) = 0;
+};
+
+#endif /* ISUBSCRIBER_HPP */
