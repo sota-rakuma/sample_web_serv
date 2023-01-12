f2d2d5762003ebeff2a09f65d2d196dbfe322179
srakuma <robben2159@gmail.com>
2023-01-12 13:51:05 +0900

    add body buffer size attribute

--- config/ServerConfig.cpp
+++ config/ServerConfig.cpp
@@ -1,12 +1,14 @@
 #include "ServerConfig.hpp"
 
 ServerConfig::ServerConfig()
-:_max_body_size(8192)
+:_max_body_size(8192),
+_body_buffersize(1024)
 {
 }
 
 ServerConfig::ServerConfig(const ServerConfig & another)
-:_max_body_size(another._max_body_size)
+:_max_body_size(another._max_body_size),
+_body_buffersize(another._body_buffersize)
 {
 }
 
@@ -19,3 +21,19 @@ size_t ServerConfig::getMaxBodySize() const
 	return _max_body_size;
 }
 
+size_t ServerConfig::getBodyBufferSize() const
+{
+	return _body_buffersize;
+}
+
+ServerConfig & ServerConfig::setMaxBodySize(size_t size)
+{
+	_max_body_size = size;
+	return *this;
+}
+
+ServerConfig & ServerConfig::setBodyBufferSize(size_t size)
+{
+	_body_buffersize = size;
+	return *this;
+}
--- config/ServerConfig.hpp
+++ config/ServerConfig.hpp
@@ -11,11 +11,15 @@ private:
 	location
 */
 	size_t _max_body_size;
+	size_t _body_buffersize;
 public:
 	ServerConfig();
 	ServerConfig(const ServerConfig &);
 	~ServerConfig();
 	size_t getMaxBodySize() const;
+	size_t getBodyBufferSize() const;
+	ServerConfig & setMaxBodySize(size_t);
+	ServerConfig & setBodyBufferSize(size_t);
 };
 
 #endif /* SERVERCONFIG_HPP */
