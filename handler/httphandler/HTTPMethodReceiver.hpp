e494745ae6ed51c05e99d21b12b5dca1334b8266
srakuma <robben2159@gmail.com>
2023-01-13 14:52:16 +0900

    add source file

--- handler/httphandler/HTTPMethodReceiver.hpp
+++ handler/httphandler/HTTPMethodReceiver.hpp
@@ -12,8 +12,13 @@ enum Stat {
 class HTTPMethodReceiver : public IOEventHandler
 {
 public:
-	HTTPMethodReceiver(/* args */);
-	~HTTPMethodReceiver();
+	HTTPMethodReceiver();
+	HTTPMethodReceiver(
+		ISubject *,
+		std::list<ICommand *> *
+	);
+	HTTPMethodReceiver(const HTTPMethodReceiver &);
+	virtual ~HTTPMethodReceiver();
 	virtual int httpGet() = 0;
 	virtual int httpPost() = 0;
 	virtual int httpDelete() = 0;
