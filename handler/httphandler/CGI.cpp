2a46c35c596807617257a094e15fe009ca31d638
srakuma <robben2159@gmail.com>
2023-01-13 15:56:27 +0900

    fix CGI

--- handler/httphandler/CGI.cpp
+++ handler/httphandler/CGI.cpp
@@ -2,28 +2,29 @@
 #include <unistd.h>
 
 CGI::CGI()
-:_get(new Get(this)),
-_post(new Post(this)),
-_delete(new Delete(this))
+:_read(new Read(this)),
+_write(new Write(this))
 {
 }
 
 
 CGI::CGI(
+	ISubject * subject,
+	std::list<ICommand *> * commands,
 	const std::string & path
 )
-:_get(new Get(this)),
-_post(new Post(this)),
-_delete(new Delete(this)),
+:HTTPMethodReceiver(subject, commands),
+_read(new Read(this)),
+_write(new Write(this)),
 _is_exist(false),
 _path(path)
 {
 }
 
 CGI::CGI(const CGI & another)
-:_get(new Get(this)),
-_post(new Post(this)),
-_delete(new Delete(this)),
+:HTTPMethodReceiver(another.getSubject(), another.getCommandList()),
+_read(new Read(this)),
+_write(new Write(this)),
 _is_exist(false),
 _path(another._path)
 {
@@ -31,26 +32,69 @@ _path(another._path)
 
 CGI::~CGI()
 {
-	delete _get;
-	delete _post;
-	delete _delete;
+	delete _read;
+	delete _write;
 }
 
-int CGI::httpPost()
+void CGI::update(int event)
 {
-	if (_is_exist == false) {
-		executeCGI(OUT);
-		_is_exist = true;
-		return 1;
+	if (event == IN) {
+		getCommandList()->push_back(_read);
+	} else {
+		getCommandList()->push_back(_write);
 	}
-	ssize_t nb = write(_out_fd, _buff.c_str(), _buff.size());
+}
+
+int CGI::read()
+{
+	char buff[BUFSIZE];
+	ssize_t nb = ::read(_in_fd, buff, BUFSIZE);
+	if (nb < 0) {
+		return -1;
+	} else if (nb == 0) {
+		// レスポンス作成フェーズ
+		return 0;
+	}
+	buff[nb] = '\0';
+	_buff += buff;
+	return 1;
+}
+
+int CGI::write()
+{
+	ssize_t nb = ::write(_out_fd, _buff.c_str(), _buff.size());
+	if (nb == -1) {
+		return -1;
+	}
 	if (nb < _nb + _buff.size()) {
 		_nb += nb;
 		return 1;
 	}
-	return httpGet();
+	return 0;
+}
+
+int CGI::httpPost()
+{
+	executeCGI(OUT);
+	getSubject()->subscribe(_out_fd, OUT, this);
+	getSubject()->subscribe(_in_fd, IN, this);
+	return 0;
 }
 
 void CGI::executeCGI(int event)
 {
+}
+
+int CGI::getInFd() const
+{
+	return _in_fd;
+}
+
+int CGI::getOutFd() const
+{
+	return _out_fd;
+}
+
+const std::string & CGI::getPath() const {
+	return _path;
 }
