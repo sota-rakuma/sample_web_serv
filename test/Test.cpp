0eb9376e97953dd59a6c9bd19df517e39b701774
mhida <mhida@mhidanoMacBook-ea.local>
2023-01-23 19:00:00 +0900

    a

--- test/Test.cpp
+++ test/Test.cpp
@@ -3,15 +3,8 @@
 int main(void)
 {
 	//TargetParserTest();
-<<<<<<< HEAD
-	//requestLineTest();
-	headerFieldTest();
-	return 0;
-}
-=======
 	// requestLineTest();
 	ConfigParserTest();
 }
 // ↓ConfigParserTest時のコンパイル用↓
 // c++ ConfigParserTest.cpp ../parser/ConfigParser.cpp ../config/ServerConfig.cpp ../config/ServerConfigFinder.cpp ../parser/TargetParser.cpp ../parser/Parser.cpp ../utils/isAlpha.cpp ../utils/isLowerCase.cpp ../utils/isUpperCase.cpp ../utils/isHexDig.cpp ../utils/isDigit.cpp Test.cpp -o cp
->>>>>>> feat_mhida
